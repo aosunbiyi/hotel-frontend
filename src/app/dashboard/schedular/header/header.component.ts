af894a200bee39849666b045d841fbe2bd02627b
aosunbiyi <adebayo.osunbiyi.a@gmail.com>
2018-07-27 06:28:57 +0100

    implementation of Schedular

--- src/app/dashboard/schedular/header/header.component.ts
+++ src/app/dashboard/schedular/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalDate, LocalTime } from 'js-joda';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  labels: any[] = [];
+
+  constructor() {
+    this.labels.push('+/-');
+    this.labels.push('Rooms');
+    const date = LocalDate.now();
+    const totalDays = date.lengthOfMonth();
+    const totalNeededDays = 15;
+    const startingDay = date.dayOfMonth();
+    const endingDay = startingDay + totalNeededDays;
+
+    // if(endingDay > (totalDays+startingDay)){
+    //   endingDay=totalDays;
+    // }
+
+    console.log(endingDay);
+
+  }
 
   ngOnInit() {
   }
