1d4f92e5e763e58607f465f01335148f74a3507c
aosunbiyi <adebayo.osunbiyi.a@gmail.com>
2018-08-20 06:44:08 +0100

    Implementation of Walkin3

--- src/app/Components/hotel-walkin/hotel-walkin.component.ts
+++ src/app/Components/hotel-walkin/hotel-walkin.component.ts
@@ -14,6 +14,12 @@ export class HotelWalkinComponent implements OnInit {
   rooms: any[] = [];
   rateTypes: any[] = [];
   rates: any[] = [];
+  wings: any[] = [];
+  floors: any[] = [];
+  taxes: any[] = [];
+  businessSourceTypes: any[] = [];
+  businessSources: any[] = [];
+
 
   walkins: Walkin[] = [
     { id: 1, guestName: 'fred amata', identity: 'passport', roomType: 'Special', room: 'Room B', rate: 3450 },
@@ -38,6 +44,21 @@ export class HotelWalkinComponent implements OnInit {
     this.ds.getRateTypes().subscribe(rt => {
       this.rateTypes = rt.rateTypes;
     });
+
+    this.ds.getWings().subscribe(w => {
+      this.wings = w.wings;
+    });
+
+    this.ds.getAllTax().subscribe(t => {
+      this.taxes = t.taxes;
+    });
+
+    this.ds.getAllBusinessSourceTypes().subscribe(b => {
+      this.businessSourceTypes = b.businessSourceTypes;
+      console.log(this.businessSourceTypes);
+    });
+
+
   }
 
   ngOnInit() {
@@ -59,6 +80,24 @@ export class HotelWalkinComponent implements OnInit {
   onRateTypeChanged(event) {
     this.ds.getRateById(event.target.value).subscribe(rms => {
       this.rates = rms.rates;
+    });
+  }
+
+  onWingChanged(event) {
+    this.ds.getFloorByWingId(event.target.value).subscribe(rms => {
+      this.floors = rms.floors;
+    });
+  }
+
+  onFloorChanged(event) {
+    this.ds.getRoomByFloorID(event.target.value).subscribe(rms => {
+      this.rooms = rms.rooms;
+    });
+  }
+
+  onBusinessSourceChanged(event) {
+    this.ds.getBusinessSourceByTypeId(event.target.value).subscribe(rms => {
+      this.businessSources = rms.businessSources;
     });
   }
 
--- src/app/backend/data.service.ts
+++ src/app/backend/data.service.ts
@@ -48,4 +48,41 @@ export class DataService {
       return response;
     }));
   }
+
+  getWings(): Observable<any> {
+    return this.http.get(`${environment.baseUrl}Wings`).pipe(map((response: any) => {
+      return response;
+    }));
+  }
+
+  getFloorByWingId(id): Observable<any> {
+    return this.http.get(`${environment.baseUrl}Floors/get_floors_wing_id/${id}`).pipe(map((response: any) => {
+      return response;
+    }));
+  }
+
+  getRoomByFloorID(id): Observable<any> {
+    return this.http.get(`${environment.baseUrl}Rooms/get_room_by_floor_id/${id}`).pipe(map((response: any) => {
+      return response;
+    }));
+  }
+
+  getAllTax(): Observable<any> {
+    return this.http.get(`${environment.baseUrl}Taxes`).pipe(map((response: any) => {
+      return response;
+    }));
+  }
+
+  getAllBusinessSourceTypes(): Observable<any> {
+    return this.http.get(`${environment.baseUrl}BusinessSourceTypes`).pipe(map((response: any) => {
+      return response;
+    }));
+  }
+
+  getBusinessSourceByTypeId(id): Observable<any> {
+    return this.http.get(`${environment.baseUrl}BusinessSources/get_business_source_by_type_id/${id}`).pipe(map((response: any) => {
+      return response;
+    }));
+  }
+
 }
